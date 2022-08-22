2179737eb46757077ca8512b791d30d46b6c5a57
danikvh <danivicen@gmail.com>
2022-08-22 22:52:55 +0200

    API call

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+// Non asynchornous function, using promises
+/* function getWeather(location) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=c33f948099d03df8b1e9c1af37f03a89`)
+    .then(function(response) {
+    console.log(response)
+    })
+    .catch(function(err) {
+    console.log(err)
+    });
+} */
 
-fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=c33f948099d03df8b1e9c1af37f03a89')
-.then(function(response) {
-  console.log(response)
-})
-.catch(function(err) {
-  console.log(err)
-});
+async function getWeather(location) {
+    try {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=c33f948099d03df8b1e9c1af37f03a89`)
+        console.log(response)
+    } catch (error) {
+        console.log(Error("Error"))
+    }
+}
+
+getWeather("London")
