fc6917a258a201ab909bfa15b19130c1c3fb0514
danikvh <danivicen@gmail.com>
2022-08-25 17:14:46 +0200

    Collect needed data

--- src/modules/api.js
+++ src/modules/api.js
@@ -18,9 +18,17 @@ async function getAPI(location) {
     try {
         const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=c33f948099d03df8b1e9c1af37f03a89`, {mode: "cors"})
         const data = await response.json()
-        console.log(data)
 
         let result = {};
+        result.clouds = data.clouds
+        result.main = data.main
+        result.name = data.name
+        result.rain = data.rain
+        result.sunrise = data.sys.sunrise
+        result.sunset = data.sys.sunset
+        result.timezone = data.timezone
+        result.visibility = data.visibility
+        result.weather = data.weather
         result.wind = data.wind
 
         localStorage.setItem("weatherData", JSON.stringify(data))
