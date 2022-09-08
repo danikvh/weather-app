655091cf01fbb32f89b36ad0132ad32ff1934c15
danikvh <danivicen@gmail.com>
2022-09-08 17:35:17 +0200

    Info boxes HTML

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -3,6 +3,12 @@ import getWeather from "./api.js"
 export default class UI {
     static async loadPage() {
         let data = await getWeather("London")
-        console.log(data)        
+        console.log(data)  
+        this.showInfo(data)
+    }
+
+    static showInfo(data) {
+        document.getElementById("clouds").textContent = 
+            "Clouds: " + data.clouds.all
     }
 }
